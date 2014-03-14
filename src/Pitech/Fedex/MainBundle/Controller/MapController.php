bee6a82e51b3cca7c19a9491b6d0ceb5e468a017
Casvan Marcel <mcasvan@pitechnologies.ro>
2014-03-15 00:02:43 +0200

    Added all gpx bus lines.

--- src/Pitech/Fedex/MainBundle/Controller/MapController.php
+++ src/Pitech/Fedex/MainBundle/Controller/MapController.php
@@ -34,9 +34,10 @@ class MapController extends Controller
                     }
                 }
             }
+
             return $times;
         }
-        throw new NotFoundResourceException('cannot read times file for ['.$name.']');
+        throw new NotFoundResourceException('cannot read times file for [' . $name . ']');
     }
 
     public function getKMLfileAction()
@@ -45,55 +46,75 @@ class MapController extends Controller
         $path = $kernel->locateResource('@AdmeDemoBundle/path/to/file/Foo.txt');
     }
 
-    public function parseGPXAction($filename)
+    public function parseGPXAction()
     {
-        $kernel = $this->container->get('kernel');
-        $path = $kernel->locateResource('@PitechFedexMainBundle/Resources/routes/mures/targumures/' . $filename);
-        if (!$xml = simplexml_load_file($path)) {
-            echo 'unable to load XML file';
-        } else {
-            $busNr = explode('.', $filename)[0];
-            /** @var EntityManager $em */
-            $em = $this->getDoctrine()->getManager();
-            $exists = $em->getRepository('PitechFedexMainBundle:Bus')->findOneBy(array('busNr' => $busNr));
-            if($exists == null){
-                $route = new Route();
-                $route->setDescription($filename);
+        $routes = array(
+            'linia4.gpx',
+//          'linia16.gpx',
+//          'linia17.gpx',
+            'linia18.gpx',
+            'linia19.gpx',
+            'linia20.gpx',
+            'linia23.gpx',
+//          'linia26.gpx',
+            'linia27.gpx',
+            'linia43.gpx',
+        );
 
-                $coords = array();
-                foreach($xml->children()->rte->rtept as $key => $coord){
-                    /** @var $coord \SimpleXMLElement */
-                    $tmpCoord = array();
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
 
-                    foreach($coord->attributes() as $key2=>$val2){
-                        /** @var $val2 \SimpleXMLElement */
-                        $tmpCoord[$key2]=(string)$val2;
+
+        $response = array();
+
+        foreach ($routes as $xmlRoute) {
+            $kernel = $this->container->get('kernel');
+            $path = $kernel->locateResource('@PitechFedexMainBundle/Resources/routes/mures/targumures/' . $xmlRoute);
+            if (!$xml = simplexml_load_file($path)) {
+                echo 'unable to load XML file';
+            } else {
+                $busNr = explode('.', $xmlRoute)[0];
+                $exists = $em->getRepository('PitechFedexMainBundle:Bus')->findOneBy(array('busNr' => $busNr));
+                if ($exists == null) {
+                    $route = new Route();
+                    $route->setDescription($xmlRoute);
+
+                    $coords = array();
+                    foreach ($xml->children()->rte->rtept as $key => $coord) {
+                        /** @var $coord \SimpleXMLElement */
+                        $tmpCoord = array();
+
+                        foreach ($coord->attributes() as $key2 => $val2) {
+                            /** @var $val2 \SimpleXMLElement */
+                            $tmpCoord[$key2] = (string)$val2;
+                        }
+                        $coords[] = $tmpCoord;
                     }
-                    $coords[] = $tmpCoord;
-                }
-                $route->setOras('Targu Mures');
-                $route->setJudet('Mures');
-                $route->setCoords($coords);
+                    $route->setOras('Targu Mures');
+                    $route->setJudet('Mures');
+                    $route->setCoords($coords);
 
 
-                $bus = new Bus();
-                $bus->setBusNr($busNr);
-                $bus->setOrePlecareSaptCapA($this->getTimesAction($busNr.'A'));
-                $bus->setOrePlecareSaptCapB($this->getTimesAction($busNr.'B'));
-                $bus->setRoute($route);
-                $route->setBus($bus);
+                    $bus = new Bus();
+                    $bus->setBusNr($busNr);
+                    $bus->setOrePlecareSaptCapA($this->getTimesAction($busNr . 'A'));
+                    $bus->setOrePlecareSaptCapB($this->getTimesAction($busNr . 'B'));
+                    $bus->setRoute($route);
+                    $route->setBus($bus);
 
 //            die(var_dump($route));
 
-                $em->persist($route);
-                $em->persist($bus);
-                $em->flush();
+                    $em->persist($route);
+                    $em->persist($bus);
+                    $em->flush();
 
-                return new Response('Route '.$filename . ' was saved.');
-            }else{
-                return new Response('Route '.$filename . ' already exists.');
+                    $response[] = 'Route ' . $xmlRoute . ' was saved.<br>';
+                } else {
+                    $response[] = 'Route ' . $xmlRoute . ' already exists.<br>';
+                }
             }
         }
+        return new Response(implode($response));
     }
 
 }
